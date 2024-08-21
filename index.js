6259ec9bb54034b4b761e041507e79531a85f43f
Mr Jabes <jabessilva31@gmail.com>
2024-08-21 00:39:46 -0300

    Ciação do CSS, ajustes de logica,  ajuste do index

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-import PetCard from "./pet-card/petCard";
+import PetCard from "./pet-card/petCard.js";
 
 window.customElements.define ("pet-card",PetCard);
--- pet-card/petCard.js
+++ pet-card/petCard.js
@@ -3,15 +3,15 @@ const template = document.createElement("template");
 template.innerHTML = `
     <div class="pet-card">
         <div class="avatar">
-            <img />
+            <img> </img>
         </div>
         <div class="detalhes">
             <h2></h2>
-            <div class="info">
-                <p>Breed: <slot name="Beagle"></slot></p>
+            <div class="informocoes">
+                <p>Raça: <slot name="Beagle"></slot></p>
                 <p>Idade: <slot name="idade"></slot></p>
             </div>
-            <div class="actions">
+            <div class="acoes">
                 <button id="greet">Olá</button>
                 <button id="toggle">Mostrar detalhes</button>
             </div>
@@ -19,10 +19,22 @@ template.innerHTML = `
     </div>
 `;
 
+
 export default class PetCard extends HTMLElement {
     constructor() {
         super();
         this.attachShadow({ mode: "open" });
-        this.shadowRoot.appendChild(template.content.cloneMode(true));
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+    static get observeAtributes (){
+        return ("name", "avatar");
+    }
+    attributeChangedCallback (name, oldValue, newValue){
+        this.shadowRoot.querySelector(".detallhes h2").innerHTML = 
+            this.getAttribute("name");
+        this.shadowRoot.querySelector(".avatar img").src = 
+            this.getAttribute("avatar");
+        this.shadowRoot.querySelector(".avatar img").alt = 
+            this.getAttribute("name");
     }
 }
